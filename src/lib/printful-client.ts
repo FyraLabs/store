1332541d2c72a817b0a554fdb0319682aef4b266
lleyton <lea@fyralabs.com>
2022-04-27 19:02:22 +0000

    upgrade shit

--- src/lib/printful-client.ts
+++ src/lib/printful-client.ts
@@ -1,3 +1,7 @@
 import { PrintfulClient } from "printful-request";
 
-export const printful = new PrintfulClient(process.env.PRINTFUL_API_KEY);
+export const printful = new PrintfulClient(process.env.PRINTFUL_API_KEY, {
+  headers: {
+    Authorization: 'Bearer ' + process.env.PRINTFUL_API_KEY,
+  }
+});
